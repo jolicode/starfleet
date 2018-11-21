87d49a8f46b8f47a38fbf630fc58adde2bb014b4
alex zerah <alex.zerah@gmail.com>
2018-11-21 17:00:30 +0100

    :bug: Fix migration

--- src/Migrations/Version20181121091202.php
+++ src/Migrations/Version20181121091202.php
@@ -8,14 +8,14 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20181121084501 extends AbstractMigration
+final class Version20181121091202 extends AbstractMigration
 {
     public function up(Schema $schema) : void
     {
         // this up() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE conference CHANGE remote_id remote_id VARCHAR(255) DEFAULT NULL, CHANGE cfp_url cfp_url VARCHAR(255) DEFAULT NULL, CHANGE cfp_end_at cfp_end_at DATETIME DEFAULT NULL, CHANGE site_url site_url VARCHAR(255) DEFAULT NULL, CHANGE hash hash VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE conference ADD hash VARCHAR(255) DEFAULT NULL, CHANGE remote_id remote_id VARCHAR(255) DEFAULT NULL, CHANGE cfp_url cfp_url VARCHAR(255) DEFAULT NULL, CHANGE cfp_end_at cfp_end_at DATETIME DEFAULT NULL, CHANGE site_url site_url VARCHAR(255) DEFAULT NULL');
         $this->addSql('ALTER TABLE submit CHANGE conference_id conference_id INT DEFAULT NULL, CHANGE talk_id talk_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user CHANGE google_id google_id VARCHAR(255) DEFAULT NULL, CHANGE name name VARCHAR(255) DEFAULT NULL');
     }
@@ -25,7 +25,7 @@ final class Version20181121084501 extends AbstractMigration
         // this down() migration is auto-generated, please modify it to your needs
         $this->abortIf($this->connection->getDatabasePlatform()->getName() !== 'mysql', 'Migration can only be executed safely on \'mysql\'.');
 
-        $this->addSql('ALTER TABLE conference CHANGE remote_id remote_id VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE hash hash VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE cfp_url cfp_url VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE cfp_end_at cfp_end_at DATETIME DEFAULT \'NULL\', CHANGE site_url site_url VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci');
+        $this->addSql('ALTER TABLE conference DROP hash, CHANGE remote_id remote_id VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE cfp_url cfp_url VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE cfp_end_at cfp_end_at DATETIME DEFAULT \'NULL\', CHANGE site_url site_url VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci');
         $this->addSql('ALTER TABLE submit CHANGE conference_id conference_id INT DEFAULT NULL, CHANGE talk_id talk_id INT DEFAULT NULL');
         $this->addSql('ALTER TABLE user CHANGE google_id google_id VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci, CHANGE name name VARCHAR(255) DEFAULT \'NULL\' COLLATE utf8mb4_general_ci');
     }
