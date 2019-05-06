8876c619631b740d460f49bbd9d84587ac238f51
Mathieu Santostefano <msantostefano@jolicode.com>
2019-05-06 15:48:12 +0200

    Upgrade EasyAdmin (#25)

--- src/Controller/ConferenceController.php
+++ src/Controller/ConferenceController.php
@@ -12,14 +12,13 @@
 namespace App\Controller;
 
 use App\Entity\Conference;
-use App\Entity\Tag;
 use Symfony\Bridge\Doctrine\RegistryInterface;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ConferenceController extends Controller
+class ConferenceController extends AbstractController
 {
     protected $registry;
 
@@ -29,8 +28,7 @@ class ConferenceController extends Controller
     }
 
     /**
-     * @Route("/", name="conferences_list")
-     * @Route("/", name="homepage")
+     * @Route("/conferences", name="conferences_list")
      */
     public function listAction(Request $request)
     {
--- src/Controller/GoogleController.php
+++ src/Controller/GoogleController.php
@@ -12,11 +12,11 @@
 namespace App\Controller;
 
 use KnpU\OAuth2ClientBundle\Client\ClientRegistry;
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class GoogleController extends Controller
+class GoogleController extends AbstractController
 {
     /**
      * @Route("/connect/google", name="connect_google")
