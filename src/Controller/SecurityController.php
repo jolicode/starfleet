9696d238391002a2e92195749b91f33a24ab10ae
Mathieu Santostefano <msantostefano@jolicode.com>
2019-07-18 11:31:31 +0200

    Add logout route (#42)

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -16,12 +16,12 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
-class GoogleController extends AbstractController
+class SecurityController extends AbstractController
 {
     /**
      * @Route("/connect/google", name="connect_google")
      */
-    public function connectAction(ClientRegistry $clientRegistry)
+    public function connectGoogleAction(ClientRegistry $clientRegistry)
     {
         return $clientRegistry
             ->getClient('google')
@@ -31,7 +31,15 @@ class GoogleController extends AbstractController
     /**
      * @Route("/connect/google/check", name="connect_google_check")
      */
-    public function connectCheckAction(Request $request)
+    public function connectGoogleCheckAction(Request $request)
     {
+    }
+
+    /**
+     * @Route("/logout", name="logout", methods={"GET"})
+     */
+    public function logout()
+    {
+        throw new \Exception('Don\'t forget to activate logout in security.yaml');
     }
 }
