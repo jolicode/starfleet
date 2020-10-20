de1a9b415596d56a091d5b4103f9aafe02d5a00c
Mathieu Santostefano <msantostefano@jolicode.com>
2020-10-20 21:15:17 +0200

    Fix cs

--- src/Command/InitCommand.php
+++ src/Command/InitCommand.php
@@ -1,5 +1,14 @@
 <?php
 
+/*
+ * This file is part of the Starfleet Project.
+ *
+ * (c) Starfleet <msantostefano@jolicode.com>
+ *
+ * For the full copyright and license information,
+ * please view the LICENSE file that was distributed with this source code.
+ */
+
 namespace App\Command;
 
 use App\Entity\User;
@@ -10,7 +19,6 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
-use function Rector\TypeDeclaration\Tests\Rector\ClassMethod\ParamTypeDeclarationRector\Fixture\Dunglas\param_no_type;
 
 class InitCommand extends Command
 {
