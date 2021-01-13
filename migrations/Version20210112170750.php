f92b512828f32eb460ebb564626177df779f8e20
Hedic Guibert <guiberthedic@gmail.com>
2021-01-13 17:35:37 +0100

    fix migration

--- migrations/Version20210112170750.php
+++ migrations/Version20210112170750.php
@@ -21,10 +21,14 @@ final class Version20210112170750 extends AbstractMigration
     public function up(Schema $schema): void
     {
         $this->addSql('ALTER TABLE excluded_tag RENAME TO conference_filter');
+        $this->addSql('DROP SEQUENCE excluded_tag_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE conference_filter_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
     }
 
     public function down(Schema $schema): void
     {
         $this->addSql('ALTER TABLE conference_filter RENAME TO excluded_tag');
+        $this->addSql('DROP SEQUENCE conference_filter_id_seq CASCADE');
+        $this->addSql('CREATE SEQUENCE excluded_tag_id_seq INCREMENT BY 1 MINVALUE 1 START 1');
     }
 }
