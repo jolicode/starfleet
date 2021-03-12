7e4f27fa515feec571a1cc3c29da6c0cdb2338dc
Hedic Guibert <guiberthedic@gmail.com>
2021-03-12 14:33:58 +0100

    Prevent slack notifications when a conference has no CfP url

--- src/Command/RemindEndingCfpCommand.php
+++ src/Command/RemindEndingCfpCommand.php
@@ -48,7 +48,7 @@ class RemindEndingCfpCommand extends Command
         foreach ($conferences as $conference) {
             $remainingDays = (int) ($conference->getCfpEndAt()->diff($today)->format('%a'));
 
-            if (\in_array($remainingDays, self::REMAINING_DAYS_STEPS, true)) {
+            if (\in_array($remainingDays, self::REMAINING_DAYS_STEPS, true) && null !== $conference->getCfpUrl()) {
                 $this->eventDispatcher->dispatch(new CfpEndingSoonEvent($conference, $remainingDays));
             }
         }
